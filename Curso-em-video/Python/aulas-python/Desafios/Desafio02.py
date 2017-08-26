4cecd44c1623f8f2528569c9120eb4a740d41ff9
AlbertoAlfredo <albertoalfredo.profissional@gmail.com>
2017-08-26 18:19:51 -0400

    Primeiro commit, começo agora a subir meus exercícios nos cursos

---
+++ Curso-em-video/Python/aulas-python/Desafios/Desafio02.py
@@ -0,0 +1,4 @@
+dia=input("Digite seu dia de nascimento: ")
+mes=input("Digite seu mês de nascimento: ")
+ano=input("Digite seu ano de nascimento: ")
+print("Você nasceu no dia", dia, "de", mes, "de", ano)
---
+++ Curso-em-video/Python/aulas-python/Desafios/Desafio03.py
@@ -0,0 +1,3 @@
+n1=int(input("Digite o primeiro número: "))
+n2=int(input("Digite o segundo número: "))
+print("A soma entre {} e {} vale {}".format(n1, n2, n1 + n2))
