28b0aaa94db670430b4a8c56627f00f791a03314
AlbertoAlfredo <raioalfredo@gmail.com>
2018-11-20 14:13:31 -0200

    Reorganização dos arquivos do curso do Curso em Vídeo, exercício da Udemy sobre tratamento de dados terminado

--- udemy-python/exercicioifelse.py
+++ udemy-python/exercicioifelse.py
@@ -1,8 +1,35 @@
 nome = input("Digite o nome do aluno: ")
-notap1 = float(input("Digite a nota da primeira prova: "))
-notap2 = float(input("Digite a nota da segunda prova: "))
-presenca = int(input("Digite o total de presenças em aula: "))
 qtd_aulas = 20
+def verifica_nota():
+    erro = True
+    while erro:
+        nota = input("Digite a nota da prova: ")
+        try:
+            nota = float(nota)
+            if nota < 0 or nota > 10:
+                print("A nota deve ser maior que 0 e menor ou igual a 10")
+            else:
+                erro = False
+        except:
+            print("Formato inválido, digite a nota entre 0 e 10 e separe o decimal por ponto '.': ")
+    return nota
+
+print("\nPrimeira prova")
+notap1 = verifica_nota()
+print("\nSegunda prova")
+notap2 = verifica_nota()
+
+erro_presenca = True
+while erro_presenca:
+    presenca = input("Digite o total de presenças em aula: ")
+    try:
+        presenca = int(presenca)
+        if presenca < 0 or presenca > 20:
+            print("A presença deve ser um valor maior que 0 e menor ou igual a 20. ")
+        erro_presenca = False
+    except:
+        print("Formato inválido, a presença deve ser número inteiro.")
+
 media = (notap1 + notap2)/2
 assiduidade = float((presenca * 100)/qtd_aulas)
 
