6ef5e879fd720aeb8ba9a706f53db37ebcd7dbe8
Alberto Alfredo <raioalfredo@gmail.com>
2020-08-13 18:10:18 -0400

    so uma mexidinha

--- rocketset/react/huntweb/src/App.js
+++ rocketset/react/huntweb/src/App.js
@@ -10,6 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+        Só uma mexidinha
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
