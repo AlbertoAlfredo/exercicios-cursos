da38972b648c05f7905dad99f9448a6e7e726fd4
albertoalfredo <raioalfredo@gmail.com>
2020-12-24 13:14:43 -0300

    Limpeza do código e criação da idéia do app

--- alura/react/ceep/src/App.js
+++ alura/react/ceep/src/App.js
@@ -1,26 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import {ListaDeNotas} from "./components/ListaDeNotas"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>Hello World caralho</p>
-      </header>
-    </div>
+    <section>
+      <form>
+        <input type="Text" placeholder="Título"/>
+        <textarea placeholder="Escreva sua nota"/>
+        <button>Criar</button>
+      </form>
+      <ListaDeNotas/>
+    </section>
   );
 }
 
+//react -> lib
+//React -> Ecossistema
 export default App;
---
+++ alura/react/ceep/src/components/ListaDeNotas.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+
+export class ListaDeNotas extends Component {
+  render() {
+    return (
+      <ul>
+        <li>
+          <section>
+            <header>
+              <h3>Título</h3>
+            </header>
+            <p>Escreva sua nota</p>
+          </section>
+        </li>
+        <li>
+          <section>
+            <header>
+              <h3>Título</h3>
+            </header>
+            <p>Escreva sua nota</p>
+          </section>
+        </li>
+        <li>
+          <section>
+            <header>
+              <h3>Título</h3>
+            </header>
+            <p>Escreva sua nota</p>
+          </section>
+        </li>
+        <li>
+          <section>
+            <header>
+              <h3>Título</h3>
+            </header>
+            <p>Escreva sua nota</p>
+          </section>
+        </li>
+      </ul>
+    );
+  }
+}
